3ccb7a16c57f713e9f26c0641033ed4dbe40b7ed
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-06-08 16:21:18 +0200

    tabella

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./style.css">
+    <title>PHP HOTEL</title>
+</head>
+<body>
+    
+    <?php
+
+        $hotels = [
+
+            [
+                'name' => 'Hotel Belvedere',
+                'description' => 'Hotel Belvedere Descrizione',
+                'parking' => true,
+                'vote' => 4,
+                'distance_to_center' => 10.4
+            ],
+            [
+                'name' => 'Hotel Futuro',
+                'description' => 'Hotel Futuro Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 2
+            ],
+            [
+                'name' => 'Hotel Rivamare',
+                'description' => 'Hotel Rivamare Descrizione',
+                'parking' => false,
+                'vote' => 1,
+                'distance_to_center' => 1
+            ],
+            [
+                'name' => 'Hotel Bellavista',
+                'description' => 'Hotel Bellavista Descrizione',
+                'parking' => false,
+                'vote' => 5,
+                'distance_to_center' => 5.5
+            ],
+            [
+                'name' => 'Hotel Milano',
+                'description' => 'Hotel Milano Descrizione',
+                'parking' => true,
+                'vote' => 2,
+                'distance_to_center' => 50
+            ],
+
+        ];
+       
+    ?>
+
+
+    <pre>
+         <?php var_dump($hotels); ?>
+    </pre>
+
+    <div class="container colorPrimary">
+        <div class="row">
+            <div class="col">
+                <table class="table text-white" >
+
+                    <thead>
+                        <tr>
+
+                            <?php foreach ($hotels[0] as $key => $value){ ?>
+                            <th scope="col"><?php echo $key ?></th>
+                            <?php } ?>
+                           
+                        </tr>
+                    </thead>
+
+                    <?php foreach($hotels as $hotel){ 
+                            $parcheggio = $hotel["parking"] ? "si" : "no"
+                        
+                        ?>
+
+                    <tbody>
+                        <tr>
+                            <td><?php echo $hotel["name"] ?></td>
+                            <td><?php echo $hotel["description"] ?></td>
+                            <td><?php echo $parcheggio ?></td>
+                            <td><?php echo $hotel["vote"] ?></td>
+                            <td><?php echo $hotel["distance_to_center"] ?>
+                        </tr>
+                    </tbody>
+
+                    <?php } ?>
+                </table>
+
+            </div>
+        </div>
+       
+    </div>
+
+
+        
+   
+
+
+    
+</body>
+</html>
